12dffeab29df3e4d0bb6d0f87f5493300e4aba7c
Donal Devine <donaldevine@gmail.com>
2017-11-28 15:08:30 +0000

    Initial Checkin. SignalR with Message Hub.

---
+++ AspNetCoreSignalRAngular/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSignalRAngular
+{
+    public class MessageHub : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+
+    }
+}
